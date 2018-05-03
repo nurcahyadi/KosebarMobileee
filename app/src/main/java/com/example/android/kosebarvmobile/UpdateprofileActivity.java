35b9d8dbdc5af73753e56a660887f8e768786191
nurcahyadi <perdana.nurcahyadi@gmail.com>
2018-05-03 12:28:56 +0700

    bismillah sukses

---
+++ app/src/main/java/com/example/android/kosebarvmobile/UpdateprofileActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.kosebarvmobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UpdateprofileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_updateprofile);
+    }
+}
